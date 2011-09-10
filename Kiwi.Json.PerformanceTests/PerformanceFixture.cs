a001c2810920ce87497f6eb0eff97553bf6b0d07
Joakim Larsson <mimmertim@gmail.com>
2011-09-10 20:35:30 +0200

    Fixed build problems related to NuGet

--- Kiwi.Json.PerformanceTests/PerformanceFixture.cs
+++ Kiwi.Json.PerformanceTests/PerformanceFixture.cs
@@ -14,7 +14,6 @@ namespace Kiwi.Json.PerformanceTests
     {
         public class Post
         {
-            //public DateTime Time { get; set; }
             public string Author { get; set; }
             public string Text { get; set; }
             public List<string> Tags { get; set; }
