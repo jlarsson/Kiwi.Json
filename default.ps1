fa493a6211b1a1aac18e79bf87b19c4568458ffc
Joakim Larsson <mimmertim@gmail.com>
2012-09-12 17:22:50 +0200

    new nuget after fixes

--- default.ps1
+++ default.ps1
@@ -1,6 +1,6 @@
 properties {
 	# Update this manually for each new version
-	$pack_version = "2.1.3.0"
+	$pack_version = "2.1.5.0"
 
 	$base_dir = resolve-path .
 	$lib_dir = join-path $base_dir "pack\lib"
