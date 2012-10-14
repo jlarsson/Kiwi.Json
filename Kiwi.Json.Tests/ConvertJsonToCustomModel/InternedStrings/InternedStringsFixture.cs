4cdf1bcfeba2a85da08c4b5c2e8f712aae108a45
jlarsson <mimmertim@gmail.com>
2012-10-14 15:48:35 +0200

    Added interning string converter

---
+++ Kiwi.Json.Tests/ConvertJsonToCustomModel/InternedStrings/InternedStringsFixture.cs
@@ -0,0 +1,38 @@
+﻿using Kiwi.Json.Converters;
+using NUnit.Framework;
+
+namespace Kiwi.Json.Tests.ConvertJsonToCustomModel.InternedStrings
+{
+    [TestFixture]
+    public class InternedStringsFixture
+    {
+        [Test]
+        public void AssumeStringsAreNotInternedByDefault()
+        {
+            const string json = @"[""a"",""a"",""a""]";
+
+            var strings = JsonConvert.Parse<string[]>(json);
+
+            Assert.That(strings,Is.EquivalentTo(new []{"a","a","a"}));
+
+            Assert.That(strings[0], Is.Not.SameAs(strings[1]));
+            Assert.That(strings[0], Is.Not.SameAs(strings[2]));
+            Assert.That(strings[1], Is.Not.SameAs(strings[2]));
+        }
+
+        [Test]
+        public void InternedStringsAreAllSame()
+        {
+            const string json = @"[""a"",""a"",""a"",null]";
+
+            var strings = JsonConvert.Parse<string[]>(json, new InterningStringConverter());
+
+            Assert.That(strings, Is.EquivalentTo(new[] { "a", "a", "a",null }));
+
+            Assert.That(strings[0], Is.SameAs(strings[1]));
+            Assert.That(strings[0], Is.SameAs(strings[2]));
+
+            Assert.That(strings[3], Is.Null);
+        }
+    }
+}
--- Kiwi.Json/Converters/InterningStringConverter.cs
+++ Kiwi.Json/Converters/InterningStringConverter.cs
@@ -7,69 +7,28 @@ namespace Kiwi.Json.Converters
 {
     public class InterningStringConverter: AbstractJsonConverter
     {
+        readonly Dictionary<string, string> _interned = new Dictionary<string, string>();
         public override ITypeBuilder CreateTypeBuilder(Type type)
         {
-            return type == typeof(string) ? new StringTypeBuilder() : null;
+            return TryCreateTypeBuilder<string, string>(type, s =>
+                                                                  {
+                                                                      if (s == null)
+                                                                      {
+                                                                          return null;
+                                                                      }
+                                                                      string interned;
+                                                                      if (!_interned.TryGetValue(s,out interned))
+                                                                      {
+                                                                          interned = s;
+                                                                          _interned[interned] = interned;
+                                                                      }
+                                                                      return interned;
+                                                                  });
         }
 
         public override ITypeWriter CreateTypeWriter(Type type)
         {
             return null;
-        }
-
-        public class StringTypeBuilder : ITypeBuilder
-        {
-            readonly Dictionary<string, string> _interned = new Dictionary<string, string>();
-
-            #region ITypeBuilder Members
-
-            public IArrayBuilder CreateArrayBuilder(ITypeBuilderRegistry registry)
-            {
-                throw new NotImplementedException();
-            }
-
-            public object CreateBool(ITypeBuilderRegistry registry, bool value)
-            {
-                throw new NotImplementedException();
-            }
-
-            public object CreateDateTime(ITypeBuilderRegistry registry, DateTime value, object sourceValue)
-            {
-                throw new NotImplementedException();
-            }
-
-            public object CreateNull(ITypeBuilderRegistry registry)
-            {
-                throw new NotImplementedException();
-            }
-
-            public object CreateNumber(ITypeBuilderRegistry registry, double value)
-            {
-                throw new NotImplementedException();
-            }
-
-            public object CreateNumber(ITypeBuilderRegistry registry, long value)
-            {
-                throw new NotImplementedException();
-            }
-
-            public IObjectBuilder CreateObjectBuilder(ITypeBuilderRegistry registry)
-            {
-                throw new NotImplementedException();
-            }
-
-            public object CreateString(ITypeBuilderRegistry registry, string value)
-            {
-                string interned;
-                if (!_interned.TryGetValue(value, out interned))
-                {
-                    interned = value;
-                    _interned.Add(value, interned);
-                }
-                return interned;
-            }
-
-            #endregion
         }
     }
 }
